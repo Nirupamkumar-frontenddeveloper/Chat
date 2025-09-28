a9b7d143026f99b8da0be87d997a12d29c747522
Nirupamkumar-frontenddeveloper <nirupamkumar2040@gmail.com>
2025-09-28 22:12:28 +0530

    server fix

--- server/server.js
+++ server/server.js
@@ -56,5 +56,8 @@ app.use("/api/messages", messageRouter);
 })();
 
 // Start the server
+if(process.env.NODE_ENV !== "production"){
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+}
+export default server;
