d06e00378e702fb4a554334dddfa064aea544c08
Nirupamkumar-frontenddeveloper <nirupamkumar2040@gmail.com>
2025-09-28 22:14:08 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import { AuthContext } from '../context/AuthContext'
 const App = () => {
   const {authUser} = useContext(AuthContext)
   return (
-    <div className="min-h-screen bg-[url('./src/assets/bgImage.svg')] bg-cover bg-no-repeat">
+    <div className="min-h-screen bg-[url('/bgImage.svg')] bg-cover bg-no-repeat">
       <Toaster/>
       <Routes>
 
